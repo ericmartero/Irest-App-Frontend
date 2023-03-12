e1b389a367343a18e1aba01d0d6e321b768f5132
Àlex Pladevall Jiménez <apladevallj@ies-sabadell.cat>
2023-03-12 15:58:40 +0100

    Llamadas al endpoint de cloudinary

---
+++ src/helpers/updateImage.js
@@ -0,0 +1,21 @@
+export const updateImage = async(image) => {
+    try {
+        const formData = new FormData();
+        formData.append('upload_preset', 'curso-vue');
+        formData.append('file', image);
+
+        const url = 'https://api.cloudinary.com/v1_1/djwjh0wpw/image/upload';
+        const params = {
+            method: 'POST',
+            body: formData,
+        };
+
+        const resp = await fetch(url, params);
+        const urlImage = await resp.json();
+
+        return urlImage;
+
+    } catch (error) {
+        throw error;
+    }
+}
