882cc626c4c3c455400b8e23a6b6335c154e324c
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-03-12 17:19:01 +0100

    Creacion componente TopMenu

--- src/layouts/AdminLayout/AdminLayout.js
+++ src/layouts/AdminLayout/AdminLayout.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useAuth } from '../../hooks';
 import { LoginAdmin } from '../../pages/Admin';
+import { TopMenu } from '../../components/Admin';
 import './AdminLayout.scss';
 
 export function AdminLayout(props) {
@@ -12,7 +13,8 @@ export function AdminLayout(props) {
 
     return (
         <div>
+            <TopMenu/>
             {children}
-        </div>  
+        </div>
     )
 }
