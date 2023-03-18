c392d5e6ed008b852d80c5716703ecbe24a120cd
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-03-19 00:38:52 +0100

    Personalización página Error404

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Button } from 'primereact/button';
+import { useHistory } from "react-router-dom";
 
 export function Error404() {
+
+  const history = useHistory();
+
   return (
-    <div>
-        <h2>Error404</h2>
+    <div className="surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden">
+      <div className="flex flex-column align-items-center justify-content-center">
+        <div style={{ borderRadius: '56px', padding: '0.3rem', background: 'linear-gradient(180deg, rgba(33, 150, 243, 0.4) 10%, rgba(33, 150, 243, 0) 30%)' }}>
+          <div className="w-full surface-card py-8 px-5 sm:px-8 flex flex-column align-items-center" style={{ borderRadius: '53px' }}>
+            <span className="text-blue-500 font-bold text-3xl">404</span>
+            <h1 className="text-900 font-bold text-5xl mb-2">Página no disponible</h1>
+            <div className="text-600 mb-5">El recurso solicitado no está disponible</div>
+            <Button icon="pi pi-arrow-left" label="Volver al Panel de Control" className="p-button-text" onClick={() => history.push('/admin')} />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
