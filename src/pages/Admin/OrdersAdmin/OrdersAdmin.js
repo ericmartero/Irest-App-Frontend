1cf61ab2e492b52c642aeb52a3c563e3878db966
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-03-27 17:29:21 +0200

    Modificaciones tabla pedidos

--- src/pages/Admin/OrdersAdmin/OrdersAdmin.js
+++ src/pages/Admin/OrdersAdmin/OrdersAdmin.js
@@ -8,7 +8,7 @@ import { Tag } from 'primereact/tag';
 export function OrdersAdmin() {
 
   const { tables, getTables } = useTable();
-    const [products, setProducts] = useState([]);
+    const [tablesCrud, setTablesCrud] = useState([]);
     const [layout, setLayout] = useState('grid');
 
     useEffect(() => {
@@ -17,7 +17,7 @@ export function OrdersAdmin() {
   
     useEffect(() => {
       if (tables) {
-        setProducts(tables);
+        setTablesCrud(tables);
       }
     }, [tables]);
 
@@ -37,26 +37,26 @@ export function OrdersAdmin() {
         }
     };
 
-    const listItem = (product) => {
+    const listItem = (table) => {
         return (
             <div className="col-12">
                 <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
-                    <img className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round" src={`https://primefaces.org/cdn/primereact/images/product/${product.image}`} alt={product.name} />
+                    <img className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round" src="https://res.cloudinary.com/djwjh0wpw/image/upload/v1679927284/mesa_h2bdwt.jpg" alt={table.number} />
                     <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
                         <div className="flex flex-column align-items-center sm:align-items-start gap-3">
-                            <div className="text-2xl font-bold text-900">{product.name}</div>
-                            <Rating value={product.rating} readOnly cancel={false}></Rating>
+                            <div className="text-2xl font-bold text-900">Mesa {table.number}</div>
+                            <Rating value={table.rating} readOnly cancel={false}></Rating>
                             <div className="flex align-items-center gap-3">
                                 <span className="flex align-items-center gap-2">
                                     <i className="pi pi-tag"></i>
-                                    <span className="font-semibold">{product.category}</span>
+                                    <span className="font-semibold">{table.category}</span>
                                 </span>
-                                <Tag value={product.inventoryStatus} severity={getSeverity(product)}></Tag>
+                                <Tag value={table.inventoryStatus} severity={getSeverity(table)}></Tag>
                             </div>
                         </div>
                         <div className="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
-                            <span className="text-2xl font-semibold">${product.price}</span>
-                            <Button icon="pi pi-shopping-cart" className="p-button-rounded" disabled={product.inventoryStatus === 'OUTOFSTOCK'}></Button>
+                            <span className="text-2xl font-semibold">${table.price}</span>
+                            <Button icon="pi pi-shopping-cart" className="p-button-rounded" disabled={table.inventoryStatus === 'OUTOFSTOCK'}></Button>
                         </div>
                     </div>
                 </div>
@@ -64,25 +64,25 @@ export function OrdersAdmin() {
         );
     };
 
-    const gridItem = (product) => {
+    const gridItem = (table) => {
         return (
             <div className="col-12 sm:col-6 lg:col-12 xl:col-4 p-2">
                 <div className="p-4 border-1 surface-border surface-card border-round">
                     <div className="flex flex-wrap align-items-center justify-content-between gap-2">
                         <div className="flex align-items-center gap-2">
                             <i className="pi pi-tag"></i>
-                            <span className="font-semibold">{product.category}</span>
+                            <span className="font-semibold">{table.category}</span>
                         </div>
-                        <Tag value={product.inventoryStatus} severity={getSeverity(product)}></Tag>
+                        <Tag value={table.inventoryStatus} severity={getSeverity(table)}></Tag>
                     </div>
                     <div className="flex flex-column align-items-center gap-3 py-5">
-                        <img className="w-9 shadow-2 border-round" src="https://res.cloudinary.com/djwjh0wpw/image/upload/v1679926080/Mesa_f3ngdw.jpg" alt={product.name} />
-                        <div className="text-2xl font-bold">{product.name}</div>
-                        <Rating value={product.rating} readOnly cancel={false}></Rating>
+                        <img className="w-9 shadow-2 border-round" src="https://res.cloudinary.com/djwjh0wpw/image/upload/v1679927284/mesa_h2bdwt.jpg" alt={table.number} />
+                        <div className="text-2xl font-bold">Mesa {table.number}</div>
+                        <Rating value={table.rating} readOnly cancel={false}></Rating>
                     </div>
                     <div className="flex align-items-center justify-content-between">
-                        <span className="text-2xl font-semibold">${product.price}</span>
-                        <Button icon="pi pi-shopping-cart" className="p-button-rounded" disabled={product.inventoryStatus === 'OUTOFSTOCK'}></Button>
+                        <span className="text-2xl font-semibold">${table.price}</span>
+                        <Button icon="pi pi-shopping-cart" className="p-button-rounded" disabled={table.inventoryStatus === 'OUTOFSTOCK'}></Button>
                     </div>
                 </div>
             </div>
@@ -108,7 +108,7 @@ export function OrdersAdmin() {
 
     return (
         <div className="card">
-            <DataView value={products} itemTemplate={itemTemplate} layout={layout} header={header()} />
+            <DataView value={tablesCrud} itemTemplate={itemTemplate} layout={layout} header={header()} />
         </div>
     )
 }
