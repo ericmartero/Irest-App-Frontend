207936ea3c4f259f7ca31a9c3bb50d7ea27d9355
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-03-29 18:40:12 +0200

    Implementación dissenyo para mostrar pedidos

--- src/pages/Admin/TableDetailsAdmin/TableDetailsAdmin.js
+++ src/pages/Admin/TableDetailsAdmin/TableDetailsAdmin.js
@@ -1,6 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { useOrder, useTable } from '../../../hooks';
 import { useParams } from 'react-router-dom';
+import { Button } from 'primereact/button';
+import { DataView } from 'primereact/dataview';
+import { Dropdown } from 'primereact/dropdown';
+import { Rating } from 'primereact/rating';
+import { Tag } from 'primereact/tag';
 
 export function TableDetailsAdmin() {
 
@@ -9,8 +14,15 @@ export function TableDetailsAdmin() {
   const { tables, getTableById } = useTable();
 
   const [table, setTable] = useState(null);
-  const [order, setOrder] = useState(null);
+  const [ordersBooking, setOrdersBooking] = useState([]);
 
+  const [sortKey, setSortKey] = useState('');
+  const [sortOrder, setSortOrder] = useState(0);
+  const [sortField, setSortField] = useState('');
+  const sortOptions = [
+    { label: 'Price High to Low', value: '!price' },
+    { label: 'Price Low to High', value: 'price' }
+  ];
 
   useEffect(() => {
     getTableById(tableURL.id);
@@ -30,13 +42,74 @@ export function TableDetailsAdmin() {
 
   useEffect(() => {
     if (orders) {
-      setOrder(orders);
+      setOrdersBooking(orders);
     }
   }, [orders]);
 
+  const getSeverity = (product) => {
+    switch (product.inventoryStatus) {
+      case 'INSTOCK':
+        return 'success';
+
+      case 'LOWSTOCK':
+        return 'warning';
+
+      case 'OUTOFSTOCK':
+        return 'danger';
+
+      default:
+        return null;
+    }
+  };
+
+  const onSortChange = (event) => {
+    const value = event.value;
+
+    if (value.indexOf('!') === 0) {
+      setSortOrder(-1);
+      setSortField(value.substring(1, value.length));
+      setSortKey(value);
+    } else {
+      setSortOrder(1);
+      setSortField(value);
+      setSortKey(value);
+    }
+  };
+
+  const header = () => {
+    return <Dropdown options={sortOptions} value={sortKey} optionLabel="label" placeholder="Sort By Price" onChange={onSortChange} className="w-full sm:w-14rem" />;
+  };
+
+  const itemTemplate = (order) => {
+    return (
+      <div className="col-12">
+        <div className="flex flex-column xl:flex-row xl:align-items-start p-4 gap-4">
+          <img className="w-9 sm:w-16rem xl:w-10rem shadow-2 block xl:block mx-auto border-round" src={order.product.image} alt={order.product.title} />
+          <div className="flex flex-column sm:flex-row justify-content-between align-items-center xl:align-items-start flex-1 gap-4">
+            <div className="flex flex-column align-items-center sm:align-items-start gap-3">
+              <div className="text-2xl font-bold text-900">{order.product.title}</div>
+              <Rating value={order.rating} readOnly cancel={false}></Rating>
+              <div className="flex align-items-center gap-3">
+                <span className="flex align-items-center gap-2">
+                  <i className="pi pi-tag"></i>
+                  <span className="font-semibold">{order.category}</span>
+                </span>
+                <Tag value={order.inventoryStatus} severity={getSeverity(order)}></Tag>
+              </div>
+            </div>
+            <div className="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
+              <span className="text-2xl font-semibold">${order.price}</span>
+              <Button icon="pi pi-shopping-cart" className="p-button-rounded" disabled={order.inventoryStatus === 'OUTOFSTOCK'}></Button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
-    <div>
-        <h2>Detalle de la mesa...</h2>
+    <div className="card">
+      <DataView value={ordersBooking} itemTemplate={itemTemplate} header={header()} sortField={sortField} sortOrder={sortOrder} />
     </div>
   )
 }
