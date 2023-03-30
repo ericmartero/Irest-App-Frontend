d69862f0b9220652625bb130026c4769500d6b7a
Àlex Pladevall Jiménez <apladevallj@ies-sabadell.cat>
2023-03-30 20:42:00 +0200

    Petición de borrar un pedido

--- src/api/order.js
+++ src/api/order.js
@@ -1,18 +1,16 @@
 const HOST_API = process.env.REACT_APP_HOST_API;
 
-export const getOrdersByTableApi = async(idTableBooking, token, status='') => {
+export const getOrdersByTableApi = async(idTableBooking, token, status = '') => {
     try {
         const tableFilter = `table=${idTableBooking}`;
         const statusFilter = `status=${status}`;
         const closeFilter = `close=false`;
 
-        let url;
+        let url = '';
 
         if (status === '') {
             url = `${HOST_API}/api/orders/?${tableFilter}&${closeFilter}`;
-        }
-
-        else {
+        } else {
             url = `${HOST_API}/api/orders/?${tableFilter}&${statusFilter}&${closeFilter}`;
         }
         
@@ -75,6 +73,26 @@ export const addOrderToTableApi = async(idTableBooking, idProduct, token) => {
         const resp = await fetch(url, params);
         await resp.json();
 
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const deleteOrderApi = async(idOrder, token) => {
+    try {
+        const url = `${HOST_API}/api/orders/${idOrder}`;
+        const params = {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+
+        const resp = await fetch(url, params);
+        const result = await resp.json();
+
+        return result;
+        
     } catch (error) {
         throw error;
     }
