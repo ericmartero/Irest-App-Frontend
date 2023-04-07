c664ca4592e738796070819b73f312bf3ed13b9d
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-04-07 14:30:06 +0200

    Creación hook pagos

---
+++ src/hooks/usePayment.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { createPaymentApi } from "../api/payment";
+import { useAuth } from './';
+
+export function usePayment() {
+
+    const { auth } = useAuth();
+
+    const createPayment = async (paymentData) => {
+        try {
+            return await createPaymentApi(paymentData, auth.token);
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    return {
+        createPayment,
+    };
+}
