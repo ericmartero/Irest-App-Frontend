fcb73bd209bfa7b4dbc2e959bfcd1b7357e794ac
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-04-13 17:04:07 +0200

    Creación componente para la ruta principal

---
+++ src/pages/Admin/HomePageAdmin/HomePageAdmin.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { OrdersAdmin } from '../OrdersAdmin';
+import { ChefTableDetails } from '../TableDetailsAdmin';
+import { useAuth } from '../../../hooks';
+
+export function HomePageAdmin() {
+
+    const { auth } = useAuth();
+    
+    if (auth?.me.user.roles.includes('admin') || auth?.me.user.roles.includes('waiter')) {
+        return (
+            <OrdersAdmin/>
+        )
+    }
+
+    else {
+        return (
+            <ChefTableDetails/>
+        )
+    }
+}
