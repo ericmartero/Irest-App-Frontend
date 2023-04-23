4e924a7a8c8602f7eba4c296e287faec96c27c9d
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-04-24 00:04:55 +0200

    Implementación obtener establecimientos en el panel

--- src/pages/Admin/EstablishmentsAdmin/EstablishmentsAdmin.js
+++ src/pages/Admin/EstablishmentsAdmin/EstablishmentsAdmin.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { useEstablishment } from '../../../hooks';
 
 export function EstablishmentsAdmin() {
+
+  const { loading, establishments, getEstablishments } = useEstablishment();
+
+  const [establishmentsCrud, setEstablishmentsCrud] = useState(null);
+
+  useEffect(() => {
+    getEstablishments();
+  }, [getEstablishments])
+
+  useEffect(() => {
+    if (establishments) {
+      setEstablishmentsCrud(establishments);
+    }
+  }, [establishments])
+  
+
   return (
     <div>
         <h1>EstablishmentsAdmin</h1>
