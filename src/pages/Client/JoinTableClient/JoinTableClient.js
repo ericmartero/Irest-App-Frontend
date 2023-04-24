ca20b3c570b35948823fb1b9e7972141efaabcb0
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-04-24 19:17:55 +0200

    Creación archivo JoinTable en cliente

---
+++ src/pages/Client/JoinTableClient/JoinTableClient.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function JoinTableClient() {
+  return (
+    <div>
+        <h1>JoinTableClient</h1>
+    </div>
+  )
+}
--- src/pages/Client/JoinTableClient/index.js
+++ src/pages/Client/JoinTableClient/index.js
@@ -1,2 +1,1 @@
-export * from './HomeClient';
 export * from './JoinTableClient';
