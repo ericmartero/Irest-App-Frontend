b07ab5ed563091168cba3f61df3cbe8438e8274c
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-04-26 00:17:01 +0200

    Cambio de diseño interfaz acceso clientes a reserva

--- src/pages/Client/AccessClientTables/ReserveTableClient/ReserveTableClient.js
+++ src/pages/Client/AccessClientTables/ReserveTableClient/ReserveTableClient.js
@@ -3,6 +3,7 @@ import { useTableBooking } from '../../../../hooks';
 import { useHistory } from "react-router-dom";
 import { Button } from 'primereact/button';
 import { Toast } from 'primereact/toast';
+import '../AccessClientTables.scss';
 
 export function ReserveTableClient(props) {
 
@@ -26,11 +27,13 @@ export function ReserveTableClient(props) {
   return (
     <div>
       <Toast ref={toastError} position="bottom-center" className="toast" />
-      <div className="flex align-items-center justify-content-center h-screen">
+      <div className='image-container'>
+        <img src="https://res.cloudinary.com/djwjh0wpw/image/upload/v1682460299/ushuaia_ibiza_wxoois.jpg" alt="hyper" className='image-fill-container' />
+      </div>
+      <div className="flex align-items-center justify-content-center content-container">
         <div className="p-4 w-full lg:w-6 mx-auto">
           <div className="text-center mb-5">
-            <img src="https://res.cloudinary.com/djwjh0wpw/image/upload/v1678622048/irest-logo-white-copy_mlspdo.png" alt="hyper" height={100} className="mb-3" />
-            <div className="text-900 text-3xl font-medium mb-3">Bienvenido a {props.table?.establishment.name}</div>
+            <div className="text-900 text-3xl font-medium mb-4">Bienvenido a {props.table?.establishment.name}</div>
             <span className="text-600 font-medium line-height-3">Pulsa el botón para reservar la mesa</span>
           </div>
 
