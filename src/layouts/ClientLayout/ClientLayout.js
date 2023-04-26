67a96a5c8a4696381c0ec7eb3414fcab448d3f8e
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-04-26 20:22:24 +0200

    Adaptación layout cliente con token

--- src/layouts/ClientLayout/ClientLayout.js
+++ src/layouts/ClientLayout/ClientLayout.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { useAuth } from '../../hooks';
+import { HomeClient } from '../../pages/Client';
 import './ClientLayout.scss';
 
 export function ClientLayout(props) {
 
     const { children } = props;
+    const { authClient } = useAuth();
+
+    if (!authClient) {
+        return <HomeClient />;
+    } 
 
     return (
         <div>
