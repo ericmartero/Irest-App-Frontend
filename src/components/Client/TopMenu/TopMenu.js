fb81262c9432f6382d60b47e777fdf4691c4ef81
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-04-27 20:05:42 +0200

    Creación componente TopMenu para el layout cliente

--- src/components/Client/TopMenu/TopMenu.js
+++ src/components/Client/TopMenu/TopMenu.js
@@ -1,74 +1,32 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../../hooks';
 import { Button } from 'primereact/button';
-import { Chip } from 'primereact/chip';
-import { TieredMenu } from 'primereact/tieredmenu';
-import classNames from 'classnames';
-import { Avatar } from 'primereact/avatar';
 import './TopMenu.scss';
 
+export function TopMenu() {
 
-export function TopMenu(props) {
-
-  const { logout, auth } = useAuth();
-  const menu = useRef(null);
-
-  const renderName = () => {
-    if (auth.me?.user.firstName && auth.me?.user.lastName) {
-      return `${auth.me.user.firstName} ${auth.me.user.lastName}`;
-    }
-
-    return auth.me?.user.email;
-  };
-
-  const initialsUser = (firstName, lastName) => {
-
-    let initials;
-
-    if (lastName === null) {
-      initials = `${firstName.charAt(0)}`;
-    }
-
-    else {
-      initials = `${firstName.charAt(0)}${lastName.charAt(0)}`;
-    }
-    
-    return initials;
-  }
-
-  const items = [
-    {
-      label: 'Cerrar sesión',
-      icon: 'pi pi-fw pi-sign-out',
-      command: () => {
-        logout();
-      }
-    }
-  ];
+  const { logout } = useAuth();
 
   return (
-    <div className="layout-topbar">
-      <Link to="/admin" className="layout-topbar-logo">
-        <img src="https://res.cloudinary.com/djwjh0wpw/image/upload/v1679673058/icono-irest_gvyksj.png" alt="logo" />
-        <span>PANEL DE CONTROL</span>
-      </Link>
+    <div className="layout-topbar layout-mobile">
+      <div className="layout-topbar-left">
+        <Link to="/admin" className="layout-topbar-logo">
+          <img src="https://res.cloudinary.com/djwjh0wpw/image/upload/v1679673058/icono-irest_gvyksj.png" alt="logo" className='layout-image' />
+        </Link>
+      </div>
 
-      <button type="button" className="p-link  layout-menu-button layout-topbar-button" onClick={props.onToggleMenuClick} >
-        <i className="pi pi-bars" />
-      </button>
+      <div className="layout-topbar-center">
+        <div className='layout-table'>
+          <b>MESA 3</b>
+        </div>
+      </div>
 
-      <TieredMenu model={items} popup ref={menu} breakpoint="767px" />
-      <Avatar label={initialsUser(auth.me?.user.firstName, auth.me?.user.lastName)} className="mr-2 layout-topbar-menu-button" size="large" shape="circle"  onClick={(event) => menu.current.toggle(event)}/>
-
-      <ul className={classNames("layout-topbar-menu lg:flex origin-top", { 'layout-topbar-menu-mobile-active': props.mobileTopbarMenuActive })}>
-        <li style={{ display: "flex", alignItems: "center" }}>
-          <Chip label={renderName()} icon="pi pi-user" className="mr-3" />
-        </li>
-        <li>
-          <Button icon="pi pi-sign-out" className="p-button-rounded p-button-secondary mr-2" onClick={logout} />
-        </li>
-      </ul>
+      <div className="layout-topbar-right">
+        <Button icon="pi pi-shopping-cart" className="layout-button p-button-secondary mr-1" />
+        <Button icon="pi pi pi-bars" className="layout-button p-button-secondary mr-1" />
+        <Button icon="pi pi-sign-out" className="layout-button p-button-secondary" />
+      </div>
     </div>
   )
 }
