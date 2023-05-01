c1172c7ec84dc519763460ac7de89c5542c31640
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-01 13:46:14 +0200

    Creación componente invitación cliente a una mesa

---
+++ src/pages/Client/InviteClientTable.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { useTableBooking, useAuth } from '../../hooks';
+import { useParams, useHistory } from 'react-router-dom';
+import { ProgressSpinner } from 'primereact/progressspinner';
+import '../../scss/AlignComponent.scss';
+
+export function InviteClientTable() {
+
+  const paramsURL = useParams();
+  const history = useHistory();
+  const { join } = useAuth();
+  const { joinTable } = useTableBooking();
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await joinTable(paramsURL.id, paramsURL.key);
+        const { tokenSession } = response;
+        join(tokenSession);
+        history.push(`/client/id_table=${paramsURL.id}&key=${paramsURL.key}`);
+      } catch (error) {
+        setError(true);
+      }
+    })()
+  }, [history, join, joinTable, paramsURL.id, paramsURL.key])
+
+  if (error) {
+    return <h1>ERROR!!</h1>
+  }
+
+  return (
+    <div className="align-container-mobile">
+      <ProgressSpinner />
+    </div>
+  )
+}
