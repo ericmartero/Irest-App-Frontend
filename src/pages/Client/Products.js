9b23f7dc9cde6808b3300e0b8c2595a4857337fd
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-01 18:06:08 +0200

    Implementación petición obtención de productos

--- src/pages/Client/Products.js
+++ src/pages/Client/Products.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useCategory } from '../../hooks';
+import { useParams } from 'react-router-dom';
 
 export function Products() {
-  return (
-    <div>Products</div>
-  )
+
+    const paramsURL = useParams();
+    const { getCategoryById } = useCategory();
+    const [productsCateogry, setProductsCateogry] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const response = await getCategoryById(paramsURL.idCategory);
+            setProductsCateogry(response.products);
+        })();
+    }, [paramsURL.idCategory, getCategoryById]);
+
+    return (
+        <div>Products</div>
+    )
 }
