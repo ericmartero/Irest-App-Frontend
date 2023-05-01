56521b2a3968522e3b049645814e0e6ca56f9741
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-01 13:45:14 +0200

    Creación ruta invitación a una mesa cliente

--- src/routes/routes.client.js
+++ src/routes/routes.client.js
@@ -1,11 +1,17 @@
 import { MainLayout, ClientLayout } from '../layouts';
-import { HomeClient, Categories } from '../pages/Client';
+import { HomeClient, Categories, InviteClientTable } from '../pages/Client';
 
 const routesClient = [
     {
         path: '/:id',
         layout: MainLayout,
         component: HomeClient,
+        exact: true
+    },
+        {
+        path: '/client-invite/id_table=:id&key=:key',
+        layout: MainLayout,
+        component: InviteClientTable,
         exact: true
     },
     {
