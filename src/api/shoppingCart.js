4069004532566f519632457102d94bb3ad2929fb
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-02 17:08:25 +0200

    Implementación metodos del carrito de pedidos del cliente

---
+++ src/api/shoppingCart.js
@@ -0,0 +1,12 @@
+const PRODUCTS_SHOPPING_CART = "products_cart";
+
+export function getProductShoppingCart() {
+    const productsCart = localStorage.getItem(PRODUCTS_SHOPPING_CART);
+    return JSON.parse(productsCart || "[]");
+};
+
+export function addProductShoppingCart(idProduct) {
+    const productsCart = getProductShoppingCart();
+    productsCart.push(idProduct);
+    localStorage.setItem(PRODUCTS_SHOPPING_CART, JSON.stringify(productsCart));
+};
