af2ddc9c7392de928ee486e5e094ec51a3d20bd6
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-02 17:53:25 +0200

    Creación dialogo para el carrito de la compra del cliente

--- src/components/Client/TopMenu/TopMenu.js
+++ src/components/Client/TopMenu/TopMenu.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../../../hooks';
 import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
 import './TopMenu.scss';
 
 export function TopMenu(props) {
 
   const { logoutClient } = useAuth();
+  const [showShoppingCartDialog, setShoppingCartDialog] = useState(false);
+
+  const hideShoppingCartDialog = () => {
+    setShoppingCartDialog(false);
+  };
+
+  const onShoppingCart = () => {
+    setShoppingCartDialog(true);
+  };
 
   return (
+    <>
     <div className="layout-topbar layout-mobile">
       <div className="layout-topbar-left">
         <Link to="/admin" className="layout-topbar-logo">
@@ -23,10 +34,18 @@ export function TopMenu(props) {
       </div>
 
       <div className="layout-topbar-right">
-        <Button icon="pi pi-shopping-cart" className="layout-button p-button-secondary mr-1" />
+        <Button icon="pi pi-shopping-cart" className="layout-button p-button-secondary mr-1" onClick={onShoppingCart} />
         <Button icon="pi pi pi-bars" className="layout-button p-button-secondary mr-1" />
         <Button icon="pi pi-sign-out" className="layout-button p-button-secondary" onClick={logoutClient} />
       </div>
+
+
     </div>
+    <Dialog visible={showShoppingCartDialog} style={{ width: '32rem' }} header="Carrito" modal onHide={hideShoppingCartDialog}>
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '1rem' }}>
+          <span>hola</span>
+        </div>
+      </Dialog>
+    </>
   )
 }
