590c7ee568a47f2cede36662880f3e81aaab62f5
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-04 01:48:50 +0200

    Cambio de diseño de la cabecera con la nueva distribución

--- src/components/Client/Header/Header.js
+++ src/components/Client/Header/Header.js
@@ -1,28 +1,19 @@
-import React, { useState } from 'react';
-import { getBookingKey } from '../../../utils/constants';
-import { useParams } from 'react-router-dom';
-import { Dialog } from 'primereact/dialog';
-import { Button } from 'primereact/button';
-import QRCode from 'react-qr-code';
+import React from 'react';
+import { Badge } from 'primereact/badge';
 import './Header.scss';
 
 export function Header(props) {
 
     const { name, isMain, goBack } = props;
 
-    const paramsURL = useParams();
-    const [showTableBookingQRDialog, setShowTableBookingQRDialog] = useState(false);
-
-    const hideShowTableBookingQRDialog = () => {
-        setShowTableBookingQRDialog(false);
-    };
-
     return (
         <>
             {isMain ?
                 <div className='header-main-container'>
                     <h2>{name}</h2>
-                    <Button icon="pi pi-qrcode" className="layout-button" onClick={() => setShowTableBookingQRDialog(true)} />
+                    <i className="pi pi-shopping-cart p-overlay-badge" style={{ fontSize: '1.8rem' }}>
+                        <Badge value="2"></Badge>
+                    </i>
                 </div>
                 :
                 <div className='header-main-container'>
@@ -30,17 +21,11 @@ export function Header(props) {
                         <i className="pi pi-arrow-left" style={{ fontSize: '1rem', marginRight: '1rem' }} onClick={goBack}></i>
                         <h2>{name}</h2>
                     </div>
-                    <Button icon="pi pi-qrcode" className="layout-button" onClick={() => setShowTableBookingQRDialog(true)} />
+                    <i className="pi pi-shopping-cart p-overlay-badge" style={{ fontSize: '1.8rem' }}>
+                        <Badge value="2"></Badge>
+                    </i>
                 </div>
             }
-
-            <Dialog visible={showTableBookingQRDialog} style={{ width: '90vw' }}
-                header="Código QR de invitación" modal onHide={hideShowTableBookingQRDialog}
-                headerClassName='header_dialog_color'>
-                <div className='header-qrDialog-container'>
-                    {paramsURL && <QRCode value={`http://localhost:3000/client-invite/id_table=${paramsURL.idTable}&key=${getBookingKey()}`} />}
-                </div>
-            </Dialog>
         </>
     )
 }
