78c4af5ab6c8f9500f0a5504709495cbd7dd780c
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-06 17:33:45 +0200

    Se implementa la opción de copiar la contraseña

--- src/components/Client/TopMenu/TopMenu.js
+++ src/components/Client/TopMenu/TopMenu.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { getBookingKey } from '../../../utils/constants';
 import { Link } from 'react-router-dom';
+import { Toast } from 'primereact/toast';
 import { Button } from 'primereact/button';
 import { Dialog } from 'primereact/dialog';
 import './TopMenu.scss';
@@ -9,7 +10,21 @@ export function TopMenu(props) {
 
   const { table, idTable } = props;
 
+  const toast = useRef(null);
   const [showKeyDialog, setShowKeyDialog] = useState(false);
+
+  const showError = (error) => {
+    toast.current.show({ severity: 'error', summary: 'Operacion Fallida', detail: error.message, life: 3000 });
+  }
+
+  const handleCopyKeyToClipboard = (text) => {
+    const textToCopy = text;
+    navigator.clipboard.writeText(textToCopy).then(() => {
+      toast.current.show({ severity: 'success', summary: 'Operación Exitosa', detail: `Se ha copiado la contraseña en el portapapeles`, life: 1500 });
+    }, (error) => {
+      showError(error);
+    });
+  };
 
   const hideShowKeyDialog = () => {
     setShowKeyDialog(false);
@@ -35,12 +50,12 @@ export function TopMenu(props) {
         <Button icon="pi pi pi-bars" className="layout-button p-button-secondary" />
       </div>
 
-      <Dialog visible={showKeyDialog} style={{ width: '90vw' }} header="Contraseña de la mesa" modal
-        className='dialog-key-confirm-container' onHide={hideShowKeyDialog}>
+      <Dialog visible={showKeyDialog} style={{ width: '90vw' }} header="Contraseña de la mesa" modal className='dialog-key-confirm-container' onHide={hideShowKeyDialog}>
+        <Toast ref={toast} position="bottom-center" />
         <div className="showkey-container">
-          <div className='key-container'>
+          <div className='key-container' onClick={() => handleCopyKeyToClipboard(getBookingKey())}>
             <span style={{ fontSize: '1.5rem' }}><b>{getBookingKey()}</b></span>
-            <i className="pi pi-copy ml-2" style={{ fontSize: '1.5rem' }} />
+            <i className="pi pi-copy ml-2" style={{ fontSize: '1.5rem', color: 'var(--primary-color)' }} />
           </div>
         </div>
       </Dialog>
