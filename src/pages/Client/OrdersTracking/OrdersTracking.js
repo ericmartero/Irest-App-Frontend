bbb08d7eeffe02441a155d212e4cccfd79744f51
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-08 16:38:45 +0200

    Creación header de la página

--- src/pages/Client/OrdersTracking/OrdersTracking.js
+++ src/pages/Client/OrdersTracking/OrdersTracking.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { useParams, useHistory } from 'react-router-dom';
+import { Header } from '../../../components/Client';
 
 export function OrdersTracking() {
-  return (
-    <div>OrdersTracking</div>
-  )
+
+    const paramsURL = useParams();
+    const history = useHistory();
+    
+    const goBack = () => {
+        history.push(`/client/id_table=${paramsURL.idTable}`);
+    };
+
+    return (
+        <div className="card">
+            <Header name="Pedidos" isMain={false} goBack={goBack} paramsURL={paramsURL} />
+        </div>
+    )
 }
