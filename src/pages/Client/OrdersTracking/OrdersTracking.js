849235b976c28553c5b8afc6f8705abfc2184b45
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-08 17:26:51 +0200

    Implementación peticiónes para obtener los pedidos de la mesa

--- src/pages/Client/OrdersTracking/OrdersTracking.js
+++ src/pages/Client/OrdersTracking/OrdersTracking.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams, useHistory } from 'react-router-dom';
+import { useOrder, useTable } from '../../../hooks';
 import { Header } from '../../../components/Client';
 
 export function OrdersTracking() {
 
     const paramsURL = useParams();
     const history = useHistory();
+    const { tables, getTableClient } = useTable();
+    const { loading, orders, getOrdersByTableClient } = useOrder();
+
+    const [table, setTable] = useState(null);
+
+    useEffect(() => {
+        getTableClient(paramsURL.idTable);
+    }, [paramsURL.idTable, getTableClient]);
+
+    useEffect(() => {
+        if (tables) {
+            setTable(tables);
+        }
+    }, [tables])
+
+    useEffect(() => {
+        (async () => {
+            getOrdersByTableClient(table.tableBooking.id);
+        })();
+    }, [table, getOrdersByTableClient]);
+
+    console.log(orders);
     
     const goBack = () => {
         history.push(`/client/id_table=${paramsURL.idTable}`);
