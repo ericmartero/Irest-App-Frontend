407f65855c1c45793cbb55ba48e36550ca5ece4c
Àlex Pladevall Jiménez <apladevallj@ies-sabadell.cat>
2023-05-08 15:18:44 +0200

    Endpoints de usuario y stripe

---
+++ src/api/stripe.js
@@ -0,0 +1,23 @@
+const HOST_API = process.env.REACT_APP_HOST_API;
+
+export const checkoutStripeApi = async(dtoStripe, token) => {
+    try {
+        const url = `${HOST_API}/api/stripe`;
+        const params = {
+          method: 'POST',
+          headers: {
+              Authorization: `Bearer ${token}`,
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(dtoStripe)
+        }
+
+        const resp = await fetch(url, params);
+        const result = await resp.json();
+
+        return result;
+
+    } catch (error) {
+        throw error;
+    }
+}
--- src/api/user.js
+++ src/api/user.js
@@ -89,3 +89,93 @@ export const deleteUserApi = async(id, token) => {
         throw error;
     }
 }
+
+export const getUsersAllApi = async(token) => {
+    try {
+        const url = `${HOST_API}/api/users/all`;
+        const params = {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+
+        const resp = await fetch(url, params);
+
+        if (resp.status === 403) {
+            return { error: 'No tienes permiso para acceder a esta página' };
+        }
+        
+        return await resp.json();
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const addUserAllApi = async(dtoAddUser, token) => {
+    try {
+        const url = `${HOST_API}/api/users/all`;
+        const params = {
+            method: 'POST',
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dtoAddUser)
+        }
+
+        const resp = await fetch(url, params);
+
+        if (resp.status === 400) {
+            throw new Error('No se ha podido añadir el usuario. El correo del usuario a añadir ya existe');
+        }
+
+        return await resp.json();
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const updateUserAllApi = async(id, dtoUpdateUser, token) => {
+    try {
+        const url = `${HOST_API}/api/users/all/${id}`;
+        const params = {
+            method: 'PATCH',
+            headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dtoUpdateUser)
+        }
+
+        const resp = await fetch(url, params);
+
+        if (resp.status === 400) {
+            throw new Error('No se ha podido actualizar el usuario. El correo del usuario a actualizar ya existe');
+        }
+
+        return await resp.json();
+
+    } catch (error) {
+        throw error;
+    }
+}
+
+export const deleteUserAllApi = async(id, token) => {
+    try {
+        const url = `${HOST_API}/api/users/all/${id}`;
+        const params = {
+            method: 'DELETE',
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+
+        const resp = await fetch(url, params);
+        return await resp.json();
+
+    } catch (error) {
+        throw error;
+    }
+}
