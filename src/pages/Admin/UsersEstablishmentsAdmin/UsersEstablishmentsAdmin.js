2cba7a46e6efa8bb17710824cd25b5c6a31c5a33
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-13 01:41:15 +0200

    Se implementa la petición para obtener todos los usuarios

--- src/pages/Admin/UsersEstablishmentsAdmin/UsersEstablishmentsAdmin.js
+++ src/pages/Admin/UsersEstablishmentsAdmin/UsersEstablishmentsAdmin.js
@@ -1,6 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { useUser, useAuth } from '../../../hooks';
 
 export function UsersEstablishmentsAdmin() {
+
+  let emptyUser = {
+    email: '',
+    firstName: '',
+    lastName: '',
+    password: '',
+    isActive: true,
+    roles: [],
+  };
+
+  const { auth } = useAuth();
+  const { users, loading, error, loadingCrud, getUsersAll, addUserAll, deleteUserAll, updateUserAll } = useUser();
+
+  const [refreshTable, setRefreshTable] = useState(false);
+  const [usersTable, setUsersTable] = useState(null);
+
+  useEffect(() => {
+    getUsersAll();
+  }, [refreshTable, getUsersAll]);
+
+  useEffect(() => {
+    if (users) {
+      const filteredUsers = users.filter(user => user.id !== auth.me.user.id);
+      setUsersTable(filteredUsers);
+    }
+  }, [users, auth]);
+
+  console.log(usersTable);
+
+  const onRefresh = () => setRefreshTable((state) => !state);
+
   return (
     <div>UsersEstablishmentAdmin</div>
   )
