6adae375f1bae3cdf0160d5d1e480af15795dfd6
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-13 18:38:58 +0200

    Implementación peticiónes editar i borrar los usuarios

--- src/pages/Admin/UsersEstablishmentsAdmin/UsersEstablishmentsAdmin.js
+++ src/pages/Admin/UsersEstablishmentsAdmin/UsersEstablishmentsAdmin.js
@@ -3,6 +3,8 @@ import { useUser, useAuth } from '../../../hooks';
 import { AccessDenied } from '../../AccessdDenied';
 import { classNames } from 'primereact/utils';
 import { ProgressSpinner } from 'primereact/progressspinner';
+import { MultiSelect } from 'primereact/multiselect';
+import { InputSwitch } from "primereact/inputswitch";
 import { InputText } from 'primereact/inputtext';
 import { Dialog } from 'primereact/dialog';
 import { Button } from 'primereact/button';
@@ -23,6 +25,8 @@ export function UsersEstablishmentsAdmin() {
     roles: [],
   };
 
+  const rolesList = ['admin', 'waiter', 'chef', 'superuser', 'customer'];
+
   const dt = useRef(null);
   const toast = useRef(null);
   const { auth } = useAuth();
@@ -33,8 +37,15 @@ export function UsersEstablishmentsAdmin() {
   const [usersTable, setUsersTable] = useState(null);
   const [selectedUsers, setSelectedUsers] = useState(null);
   const [user, setUser] = useState(emptyUser);
+  const [actionName, setActionName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+  const [validationErrors, setValidationErrors] = useState({});
+  const [userDialog, setUserDialog] = useState(false);
   const [deleteUserDialog, setDeleteUserDialog] = useState(false);
   const [deleteUsersDialog, setDeleteUsersDialog] = useState(false);
+  const [isEditUser, setIsEditUser] = useState(false)
+  const [lastUserEdit, setlastUserEdit] = useState({});
+  const [selectedRoles, setSelectedRoles] = useState(null);
 
   useEffect(() => {
     getUsersAll();
@@ -55,6 +66,64 @@ export function UsersEstablishmentsAdmin() {
 
   const exportCSV = () => {
     dt.current.exportCSV();
+  };
+
+  const saveUser = async () => {
+
+    const isValid = validateFields();
+    setSubmitted(true);
+
+    if (isValid) {
+
+      const lowerCaseSelectedRoles = selectedRoles?.map(role => role.toLowerCase());
+
+      if (user.id) {
+
+        const editUser = {
+          ...(lastUserEdit.email !== user.email && { email: user.email }),
+          ...(lastUserEdit.firstName !== user.firstName && { firstName: user.firstName }),
+          ...(user.password !== '' && { password: user.password }),
+          ...(user.lastName !== '' ? { lastName: user.lastName } : { lastName: null }),
+          ...(selectedRoles.length !== 0 ? { roles: lowerCaseSelectedRoles } : { roles: ['waiter'] }),
+          ...(lastUserEdit.isActive !== user.isActive && { isActive: user.isActive }),
+        };
+
+        try {
+          await updateUserAll(user.id, editUser);
+          onRefresh();
+          toast.current.show({ severity: 'success', summary: 'Operación Exitosa', detail: `Usuario ${user.firstName} actualizado correctamente`, life: 3000 });
+        } catch (error) {
+          showError(error);
+        }
+
+      } else {
+
+        user.roles = lowerCaseSelectedRoles;
+
+        const newUser = {
+          email: user.email,
+          firstName: user.firstName,
+          password: user.password,
+          isActive: user.isActive,
+          ...(user.lastName && { lastName: user.lastName }),
+          ...(user.roles.length !== 0 ? { roles: lowerCaseSelectedRoles } : { roles: ['waiter'] })
+        };
+
+        try {
+          await addUserAll(newUser);
+          onRefresh();
+          toast.current.show({ severity: 'success', summary: 'Operacion Exitosa', detail: `Usuario ${user.firstName} creado correctamente`, life: 3000 });
+        } catch (error) {
+          showError(error);
+        }
+      }
+
+      setSubmitted(false);
+      setValidationErrors({});
+      setUserDialog(false);
+      setUser(emptyUser);
+      document.body.classList.remove('body-scroll-lock');
+    }
   };
 
   const confirmDeleteUser = (user) => {
@@ -100,6 +169,107 @@ export function UsersEstablishmentsAdmin() {
     setSelectedUsers(null);
   };
 
+  const onInputChange = (e, name) => {
+    const val = e.target.value || '';
+
+    let errors = { ...validationErrors };
+
+    switch (name) {
+      case "email":
+        if (!isValidEmail(val)) {
+          errors.email = "El formato de correo electrónico es inválido";
+        } else {
+          delete errors.email;
+        }
+        break;
+      case "firstName":
+        if (val.length < 2) {
+          errors.firstName = "El nombre tiene que tener mínimo 2 letras";
+        } else {
+          delete errors.firstName;
+        }
+        break;
+      case "password":
+        if (isEditUser)
+          if (val.length > 0 && !validatePassword(val)) {
+            errors.password = "La contraseña tiene que tener mínimo 6 caracteres, una mayúscula, una minúscula y un número"
+          }
+          else {
+            delete errors.password;
+          }
+        else {
+          if (!validatePassword(val)) {
+            errors.password = "La contraseña tiene que tener mínimo 6 caracteres, una mayúscula, una minúscula y un número"
+          }
+          else {
+            delete errors.password;
+          }
+        }
+        break;
+      default:
+        break;
+    }
+
+    setUser(prevUser => ({ ...prevUser, [name]: val }));
+    setValidationErrors(errors);
+  };
+
+  const validateFields = () => {
+    const errors = {};
+    if (!user.email) {
+      errors.email = "El email es requerido";
+    } else if (!isValidEmail(user.email)) {
+      errors.email = "El formato de correo electrónico es inválido";
+    }
+    if (!user.firstName) {
+      errors.firstName = "El nombre es requerido";
+    } else if (user.firstName.length < 2) {
+      errors.firstName = "El nombre tiene que tener mínimo 2 letras";
+    }
+
+    if (isEditUser) {
+      if (user.password.length > 0 && !validatePassword(user.password)) {
+        errors.password = "La contraseña tiene que tener mínimo 6 caracteres, una mayúscula, una minúscula y un número"
+      }
+    }
+
+    else {
+      if (!user.password) {
+        errors.password = "La contraseña es requerida";
+      } else if (!validatePassword(user.password)) {
+        errors.password = "La contraseña tiene que tener mínimo 6 caracteres, una mayúscula, una minúscula y un número"
+      }
+    }
+
+    setValidationErrors(errors);
+    return Object.keys(errors).length === 0;
+  };
+
+  const handleInputSwitch = (e, valid) => {
+    const val = e.target.value;
+    setUser(prevUser => ({ ...prevUser, [valid]: val }));
+  };
+
+  const isValidEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+  };
+
+  function validatePassword(password) {
+    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[\w,.-]{6,}$/;
+    return passwordRegex.test(password);
+  };
+
+  const editUser = (userEdit) => {
+    setSubmitted(false);
+    setIsEditUser(true);
+    setlastUserEdit(userEdit);
+    setUser({ ...userEdit, password: '' });
+    setSelectedRoles(userEdit.roles);
+    setUserDialog(true);
+    setActionName('Editar Usuario');
+  };
+
   const hideDeleteUserDialog = () => {
     setDeleteUserDialog(false);
   };
@@ -108,12 +278,39 @@ export function UsersEstablishmentsAdmin() {
     setDeleteUsersDialog(false);
   };
 
+  const hideDialog = () => {
+    setSubmitted(false);
+    setUserDialog(false);
+    setValidationErrors({});
+    document.body.classList.remove('body-scroll-lock');
+  };
+
   const confirmDeleteSelected = () => {
     setDeleteUsersDialog(true);
   };
 
+  const itemTemplate = (option) => {
+    return (
+      <div className="flex align-items-center">
+        <span>{option}</span>
+      </div>
+    );
+  };
+
   const activeBodyTemplate = (rowData) => {
     return <i className={classNames('pi', (rowData.isActive ? 'text-green-500 pi-check-circle' : 'text-red-500 pi-times-circle'))}></i>;
+  };
+
+  const selectedItemTemplate = (option) => {
+    if (option) {
+      return (
+        <div className="inline-flex align-items-center py-1 px-2 bg-primary text-primary border-round mr-2">
+          <span>{option}</span>
+        </div>
+      );
+    }
+
+    return 'Selecciona los roles';
   };
 
   const leftToolbarTemplate = () => {
@@ -132,11 +329,18 @@ export function UsersEstablishmentsAdmin() {
   const actionBodyTemplate = (rowData) => {
     return (
       <React.Fragment>
-        <Button icon="pi pi-pencil" rounded outlined className="mr-2" onClick={""} />
+        <Button icon="pi pi-pencil" rounded outlined className="mr-2" onClick={() => editUser(rowData)} />
         <Button icon="pi pi-trash" rounded outlined severity="danger" onClick={() => confirmDeleteUser(rowData)} />
       </React.Fragment>
     );
   };
+
+  const userDialogFooter = (
+    <React.Fragment>
+      <Button label="Cancelar" icon="pi pi-times" outlined onClick={hideDialog} />
+      <Button label="Guardar" icon="pi pi-check" onClick={saveUser} disabled={!submitted || Object.keys(validationErrors).length === 0 ? false : true} />
+    </React.Fragment>
+  );
 
   const deleteUserDialogFooter = (
     <React.Fragment>
@@ -225,6 +429,85 @@ export function UsersEstablishmentsAdmin() {
                   <Column body={actionBodyTemplate} exportable={false} style={{ minWidth: '10rem' }}></Column>
                 </DataTable>
               </div>
+
+              <Dialog visible={userDialog} style={{ width: '32rem' }} breakpoints={{ '960px': '75vw', '641px': '90vw' }} header={actionName} modal className="p-fluid" footer={userDialogFooter} onHide={hideDialog}>
+                {loadingCrud && <ProgressSpinner style={{ position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)', zIndex: 1 }} />}
+                <div className="field">
+                  <label htmlFor="email" className="font-bold">
+                    Email
+                  </label>
+                  <InputText id="email" type="email" value={user.email} onChange={(e) => onInputChange(e, 'email')} required autoFocus
+                    className={classNames({ "p-invalid": submitted && (!user.email || validationErrors.email) })} />
+                  {submitted && !user.email
+                    ? (<small className="p-error">El email es requerido</small>)
+                    : submitted && validationErrors.email && (<small className="p-error">{validationErrors.email}</small>)
+                  }
+                </div>
+
+                <div className="field">
+                  <label htmlFor="roles" className="font-bold">
+                    Roles
+                  </label>
+                  <MultiSelect
+                    value={selectedRoles}
+                    onChange={(e) => setSelectedRoles(e.value)}
+                    options={rolesList}
+                    optionLabel="role"
+                    placeholder="Selecciona los roles"
+                    itemTemplate={itemTemplate}
+                    selectedItemTemplate={selectedItemTemplate}
+                  />
+                </div>
+
+                <div className="field">
+                  <label htmlFor="firstName" className="font-bold">
+                    Nombre
+                  </label>
+                  <InputText id="firstName" value={user.firstName} onChange={(e) => onInputChange(e, 'firstName')} required autoFocus className={classNames({ "p-invalid": submitted && (!user.firstName || validationErrors.firstName) })} />
+                  {submitted && !user.firstName
+                    ? <small className="p-error">El nombre es requerido</small>
+                    : submitted && validationErrors.firstName && (<small className="p-error">{validationErrors.firstName}</small>)
+                  }
+                </div>
+                <div className="field">
+                  <label htmlFor="lastName" className="font-bold">
+                    Apellidos
+                  </label>
+                  <InputText id="lastName" value={user.lastName || ''} onChange={(e) => onInputChange(e, 'lastName')} />
+                </div>
+                <div className="field">
+                  <label htmlFor="password" className="font-bold">
+                    Contraseña
+                  </label>
+                  {isEditUser ? (
+                    <>
+                      <InputText id="password" type="password" value={user.password} onChange={(e) => onInputChange(e, 'password')} required autoFocus className={classNames({ "p-invalid": submitted && (validationErrors.password) })} />
+                      {submitted && validationErrors.password && (<small className="p-error">{validationErrors.password}</small>)}
+                    </>
+                  ) : (
+                    <>
+                      <InputText id="password" type="password" value={user.password} onChange={(e) => onInputChange(e, 'password')} required autoFocus className={classNames({ "p-invalid": submitted && (!user.password || validationErrors.password) })} />
+                      {submitted && !user.password
+                        ? (<small className="p-error">La contraseña es requerida</small>)
+                        : submitted && validationErrors.password && (<small className="p-error">{validationErrors.password}</small>)
+                      }
+                    </>
+                  )}
+                </div>
+
+                <div className="field" style={{ height: "2.5rem", display: "flex", alignItems: "center" }}>
+                  <div className="p-field-checkbox" style={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <InputSwitch
+                      id='isActive'
+                      checked={user.isActive}
+                      onChange={(e) => handleInputSwitch(e, 'isActive')}
+                    />
+                    <label htmlFor="isActive" className="font-bold" style={{ marginLeft: "1rem", alignSelf: "center" }}>
+                      Usuario Activo
+                    </label>
+                  </div>
+                </div>
+              </Dialog>
 
               <Dialog visible={deleteUserDialog} style={{ width: '32rem' }} breakpoints={{ '960px': '75vw', '641px': '90vw' }} header="Confirmar" modal footer={deleteUserDialogFooter} onHide={hideDeleteUserDialog}>
                 <div className="confirmation-content">
