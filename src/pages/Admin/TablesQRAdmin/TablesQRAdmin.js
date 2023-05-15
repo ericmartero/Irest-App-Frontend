db4eac66ee29eff8fe46fc074f91f2d63cd1e722
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-15 13:07:15 +0200

    Se elimina la clase de seleccionar los qr de las mesas

--- src/pages/Admin/TablesQRAdmin/TablesQRAdmin.js
+++ src/pages/Admin/TablesQRAdmin/TablesQRAdmin.js
@@ -68,7 +68,7 @@ export function TablesQRAdmin() {
     const gridItem = (table) => {
         return (
             <div className="col-12 sm:col-6 lg:col-12 xl:col-4 p-2">
-                <div className="p-4 border-1 surface-border surface-card border-round">
+                <div className="p-4 border-1 surface-border border-round">
                     <div className="flex flex-wrap align-items-center justify-content-between gap-2">
                         <div className="flex align-items-center gap-2">
                             <div className="text-2xl font-bold">Mesa {table.number}</div>
