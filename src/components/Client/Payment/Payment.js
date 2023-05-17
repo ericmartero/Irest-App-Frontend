87792bcbfb7a7e9873c03c8ffc391d2ccd31c16f
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-17 12:39:11 +0200

    Se elimina la visualización de la cuenta en el pago

--- src/components/Client/Payment/Payment.js
+++ src/components/Client/Payment/Payment.js
@@ -1,14 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useOrder, usePayment } from '../../../hooks';
 import { PAYMENT_TYPE } from '../../../utils/constants';
-import { classNames } from 'primereact/utils';
 import { Button } from 'primereact/button';
 import { Dialog } from 'primereact/dialog';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
 import { forEach, size } from 'lodash';
-import moment from 'moment';
-import 'moment/locale/es';
 import './Payment.scss';
 
 export function Payment(props) {
@@ -25,7 +20,6 @@ export function Payment(props) {
     const [showPaymentDialog, setShowPaymentDialog] = useState(false);
     const [showConfirmPaymentDialog, setShowConfirmPaymentDialog] = useState(false);
     const [finishPaymentDialog, setFinishPaymentDialog] = useState(false);
-    const [showBillDialog, setShowBillDialog] = useState(false);
 
     useEffect(() => {
         (async () => {
@@ -72,37 +66,6 @@ export function Payment(props) {
         setShowPaymentDialog(false);
     };
 
-    const hideBillDialog = () => {
-        setShowBillDialog(false);
-        setShowConfirmPaymentDialog(false);
-        setShowPaymentDialog(false);
-    };
-
-    const openDialogFinishPayment = () => {
-        setFinishPaymentDialog(true);
-        setShowBillDialog(false);
-    };
-
-    const formatCurrency = (value) => {
-        return value?.toLocaleString('es-ES', { style: 'currency', currency: 'EUR' });
-    };
-
-    const priceBodyTemplate = (rowData) => {
-        return formatCurrency(rowData.product.price * rowData.quantity);
-    };
-
-    const groupOrdersStatus = (data) => {
-        return data.reduce((acc, order) => {
-            const existingOrder = acc.find((o) => o.product.id === order.product.id);
-            if (existingOrder) {
-                existingOrder.quantity += 1;
-            } else {
-                acc.push({ id: order.id, product: order.product, status: order.status, tableBooking: order.tableBooking, quantity: 1, createdAt: order.createdAt });
-            }
-            return acc;
-        }, []);
-    };
-
     const createPayment = async () => {
 
         let totalPayment = 0;
@@ -124,7 +87,7 @@ export function Payment(props) {
 
         onRefresh();
         setShowConfirmPaymentDialog(true);
-        setShowBillDialog(true);
+        //SE ACABAAA MOSTRAR TOAST
     };
 
     const finishPayment = async () => {
@@ -159,14 +122,7 @@ export function Payment(props) {
 
     const hideFinishPaymentDialog = () => {
         setFinishPaymentDialog(false);
-        setShowBillDialog(true);
     };
-
-    const showBillDialogFooter = (
-        <div className='footerBill'>
-            <Button label="Realizar el pago" onClick={openDialogFinishPayment} className="bttnFoot" />
-        </div>
-    );
 
     const showConfirmPaymentDialogFooter = (
         <React.Fragment>
@@ -194,35 +150,6 @@ export function Payment(props) {
 
                 <div className='paymentDialog-bottom-buttons'>
                     <Button icon="pi pi-credit-card" label='Aplicación' className='paymentDialog-button' style={{ width: "49%" }} onClick={() => onPaymentDialog(PAYMENT_TYPE.CARD)} />
-                </div>
-            </Dialog>
-
-            <Dialog visible={showBillDialog} style={{ width: '90vw' }} header={`Cuenta Mesa ${table?.number}`} modal className='bill-dialog-container'
-                footer={paymentType === PAYMENT_TYPE.CARD && showBillDialogFooter} onHide={hideBillDialog}>
-                <div className='product-add-order'>
-                    <div className='product-add-info'>
-                        <span className="font-bold">{`MESA: ${table?.number}`}</span>
-                    </div>
-                    <div>
-                        <span><strong>FECHA:</strong> {moment(paymentData?.createdAt).format('DD/MM/YYYY HH:mm:ss')}</span>
-                    </div>
-                </div>
-
-                <div className='table-orders-payment' style={{ marginTop: '1.5rem' }}>
-                    <DataTable value={ordersTable && groupOrdersStatus(ordersTable)} >
-                        <Column field="quantity" header="UNIDADES" bodyStyle={{ textAlign: 'center' }}></Column>
-                        <Column field="product.title" header="PRODUCTO" bodyStyle={{ textAlign: 'center' }}></Column>
-                        <Column field="product.price" header="IMPORTE" body={priceBodyTemplate} bodyStyle={{ textAlign: 'center' }}></Column>
-                    </DataTable>
-                </div>
-
-                <div style={{ display: 'flex', alignItems: 'center', justifyContent: "space-between", marginTop: "2rem" }}>
-                    <span className="font-bold">MÉTODO DE PAGO:</span>
-                    <i className={classNames({
-                        "pi pi-credit-card": paymentData?.paymentType === PAYMENT_TYPE.CARD,
-                        "pi pi-wallet": paymentData?.paymentType === PAYMENT_TYPE.CASH
-                    })} style={{ fontSize: '1.5rem' }}></i>
-                    <span className="font-bold">TOTAL: {paymentData?.totalPayment.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} €</span>
                 </div>
             </Dialog>
 
