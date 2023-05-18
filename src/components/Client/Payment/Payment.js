61a78b163599a6aaca994caa57fcc009e02e0b4e
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-19 00:51:39 +0200

    Cambios en el dialogo de visualizar la cuenta antes de pagar

--- src/components/Client/Payment/Payment.js
+++ src/components/Client/Payment/Payment.js
@@ -4,7 +4,11 @@ import { StripePayment } from '../StripePayment';
 import { PAYMENT_TYPE } from '../../../utils/constants';
 import { Button } from 'primereact/button';
 import { Dialog } from 'primereact/dialog';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 import { forEach, size } from 'lodash';
+import moment from 'moment';
+import 'moment/locale/es';
 import '../../../scss/Dialogs.scss';
 import './Payment.scss';
 
@@ -17,14 +21,26 @@ export function Payment(props) {
 
     const [ordersTable, setOrdersTable] = useState(null);
     const [paymentType, setPaymentType] = useState(null);
+    const [totalPayment, setTotalPayment] = useState(null);
     const [showPaymentDialog, setShowPaymentDialog] = useState(false);
     const [showConfirmPaymentDialog, setShowConfirmPaymentDialog] = useState(false);
     const [showStripePaymentDialog, setShowStripePaymentDialog] = useState(false);
+    const [showAcountPaymentDialog, setShowAcountPaymentDialog] = useState(false);
 
     useEffect(() => {
         if (orders) {
             setOrdersTable(orders);
         }
+    }, [orders]);
+
+    useEffect(() => {
+        let totalPayment = 0;
+
+        forEach(orders, (order) => {
+            totalPayment += order.product.price;
+        });
+
+        setTotalPayment(Number(totalPayment.toFixed(2)));
     }, [orders]);
 
     const onShowPaymentDialog = () => {
@@ -49,6 +65,7 @@ export function Payment(props) {
 
         if (paymentType === PAYMENT_TYPE.APP) {
             setShowStripePaymentDialog(true);
+            setShowAcountPaymentDialog(false);
         }
 
         else {
@@ -76,10 +93,37 @@ export function Payment(props) {
         setShowConfirmPaymentDialog(false);
     };
 
+    const groupOrdersStatus = (data) => {
+        return data.reduce((acc, order) => {
+            const existingOrder = acc.find((o) => o.product.id === order.product.id);
+            if (existingOrder) {
+                existingOrder.quantity += 1;
+            } else {
+                acc.push({ id: order.id, product: order.product, status: order.status, tableBooking: order.tableBooking, quantity: 1, createdAt: order.createdAt });
+            }
+            return acc;
+        }, []);
+    };
+
+    const formatCurrency = (value) => {
+        return value?.toLocaleString('es-ES', { style: 'currency', currency: 'EUR' });
+    };
+
+    const priceBodyTemplate = (rowData) => {
+        return formatCurrency(rowData.product.price * rowData.quantity);
+    };
+
     const onPaymentDialog = async (paymentType) => {
         setShowPaymentDialog(false);
         setPaymentType(paymentType);
-        setShowConfirmPaymentDialog(true);
+
+        if (paymentType === PAYMENT_TYPE.APP) {
+            setShowAcountPaymentDialog(true);
+        }
+
+        else {
+            setShowConfirmPaymentDialog(true);
+        }
     };
 
     const hideShowConfirmPaymentDialog = () => {
@@ -92,6 +136,11 @@ export function Payment(props) {
         setShowStripePaymentDialog(false);
     };
 
+    const hideShowAcountPaymentDialog = () => {
+        setShowAcountPaymentDialog(false);
+        setShowPaymentDialog(true);
+    };
+
     const showConfirmPaymentDialogFooter = (
         <React.Fragment>
             <Button label="No" icon="pi pi-times" outlined onClick={hideShowConfirmPaymentDialog} style={{ marginTop: "10px" }} />
@@ -99,11 +148,10 @@ export function Payment(props) {
         </React.Fragment>
     );
 
-    const ShowStripePaymentDialogFooter = (
-        <React.Fragment>
-            <Button label="No" icon="pi pi-times" outlined onClick={hideShowStripePaymentDialog} style={{ marginTop: "10px" }} />
-            <Button label="Si" icon="pi pi-check" onClick={""} />
-        </React.Fragment>
+    const showAcountPaymentDialogFooter = (
+        <div className='footerBill'>
+            <Button label="Realizar el pago" className="bttnFoot" onClick={createPayment} />
+        </div>
     );
 
     return (
@@ -130,9 +178,38 @@ export function Payment(props) {
                 </div>
             </Dialog>
 
+            <Dialog visible={showAcountPaymentDialog} style={{ width: '90vw' }} header={`Cuenta mesa ${table?.number}`} modal onHide={hideShowAcountPaymentDialog}
+                footer={showAcountPaymentDialogFooter} className='dialog-account-container'>
+                <div className='product-add-order'>
+                    <div className='product-add-info'>
+                        <span className="font-bold">{`MESA: ${table?.number}`}</span>
+                    </div>
+                    <div>
+                        <span><strong>FECHA:</strong> {moment(payment?.createdAt).format('DD/MM/YYYY HH:mm:ss')}</span>
+                    </div>
+                </div>
+
+                <div className='table-orders-payment' style={{ marginTop: '1.5rem' }}>
+                    <DataTable value={orders && groupOrdersStatus(orders)} >
+                        <Column field="quantity" header="UNIDADES" bodyStyle={{ textAlign: 'center' }}></Column>
+                        <Column field="product.title" header="PRODUCTO" bodyStyle={{ textAlign: 'center' }}></Column>
+                        <Column field="product.price" header="IMPORTE" body={priceBodyTemplate} bodyStyle={{ textAlign: 'center' }}></Column>
+                    </DataTable>
+                </div>
+
+                <div className='mt-3' style={{display: 'flex', alignItems: 'center'}}>
+                    <span className="font-bold">MÉTODO DE PAGO:</span> <i className={"pi pi-credit-card ml-5"} style={{ fontSize: "1.5rem" }}></i>
+
+                </div>
+                <div className='mt-3'>
+                    <span className="font-bold">TOTAL A PAGAR: </span>
+                    <span className="font-bold ml-6">{totalPayment?.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} €</span>
+                </div>
+            </Dialog>
+
             <Dialog visible={showStripePaymentDialog} style={{ width: '90vw' }} header={`Pago mesa ${table?.number}`} modal onHide={hideShowStripePaymentDialog}>
                 <div className="mt-4">
-                    <StripePayment table={table} payment={payment} orders={orders} />
+                    <StripePayment />
                 </div>
             </Dialog>
         </>
