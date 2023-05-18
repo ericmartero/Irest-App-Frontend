21f1ad26e6766946c04e20467a75f607ef26cbfe
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-18 19:56:14 +0200

    Instalación i implementación de stripe en la aplicación

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Navigation } from './routes';
-import './App.scss';
 import { AuthProvider } from './context';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import './App.scss';
+
+const stripeKey = loadStripe('pk_test_51N19O8LGn95IZX0nn5qvwmGAlgcPkKnSaB7ab8B1N30ukaIZGJGl2nIGbYFD5qLuN4OXs4c7zWPQ7Q046rIaWGEs0014objLCY');
 
 function App() {
   return (
     <AuthProvider>
-      <Navigation/>
+      <Elements stripe={stripeKey}>
+        <Navigation />
+      </Elements>
     </AuthProvider>
   );
 }
