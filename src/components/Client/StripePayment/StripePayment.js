716287993eec2ff0d8d4ad21327b42703163a3fc
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-18 19:57:18 +0200

    Implementación del componente para el pago en stripe

---
+++ src/components/Client/StripePayment/StripePayment.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from 'react';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import { Button } from 'primereact/button';
+import './StripePayment.scss';
+
+export function StripePayment() {
+
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (!stripe || !elements) {
+            // Stripe no ha sido cargado todavía
+            return;
+        }
+
+        const cardElement = elements.getElement(CardElement);
+
+        // Realizar la lógica de pago con Stripe aquí
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <span>Introduce tu tarjeta de crédito:</span>
+
+            <CardElement
+                options={{
+                    style: {
+                        base: {
+                            fontSize: '16px',
+                            fontFamily: 'Arial, sans-serif',
+                        },
+                    },
+                }}
+            />
+
+            <Button type="submit" label='Realizar el pago' disabled={!stripe} />
+        </form>
+    );
+}
--- src/components/Client/index.js
+++ src/components/Client/index.js
@@ -3,3 +3,4 @@ export * from './ShoppingCart';
 export * from './Header';
 export * from './FooterMenu';
 export * from './Payment';
+export * from './StripePayment';
