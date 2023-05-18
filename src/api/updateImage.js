c435a903e94d01012e0c2fcd43ce8202f32db4d3
Àlex Pladevall Jiménez <apladevallj@ies-sabadell.cat>
2023-05-18 21:10:15 +0200

    Variables de entorno Cloudinary

--- src/api/updateImage.js
+++ src/api/updateImage.js
@@ -1,10 +1,13 @@
+const CLOUDINARY_KEY = process.env.REACT_APP_CLOUDINARY_KEY;
+const CLOUDINARY_STORAGE = process.env.REACT_APP_CLOUDINARY_STORAGE;
+
 export const updateImage = async(image) => {
     try {
         const formData = new FormData();
-        formData.append('upload_preset', 'curso-vue');
+        formData.append('upload_preset', CLOUDINARY_STORAGE);
         formData.append('file', image);
 
-        const url = 'https://api.cloudinary.com/v1_1/djwjh0wpw/image/upload';
+        const url = `https://api.cloudinary.com/v1_1/${CLOUDINARY_KEY}/image/upload`;
         const params = {
             method: 'POST',
             body: formData,
