d45c5adbbd8e259bd4e93ca5d51e2660a7e97c35
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-19 00:15:00 +0200

    Modificación componente de pago de stripe

--- src/components/Client/StripePayment/StripePayment.js
+++ src/components/Client/StripePayment/StripePayment.js
@@ -12,8 +12,8 @@ export function StripePayment(props) {
 
     const { table, payment, orders } = props;
 
-    const stripe = useStripe();
-    const elements = useElements();
+    //const stripe = useStripe();
+    //const elements = useElements();
 
     const [totalPayment, setTotalPayment] = useState(null);
 
@@ -25,20 +25,7 @@ export function StripePayment(props) {
         });
 
         setTotalPayment(Number(totalPayment.toFixed(2)));
-    }, [orders])
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-
-        if (!stripe || !elements) {
-            // Stripe no ha sido cargado todavía
-            return;
-        }
-
-        const cardElement = elements.getElement(CardElement);
-
-        // Realizar la lógica de pago con Stripe aquí
-    };
+    }, [orders]);
 
     const groupOrdersStatus = (data) => {
         return data.reduce((acc, order) => {
@@ -62,30 +49,30 @@ export function StripePayment(props) {
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <div className='product-add-order'>
-                    <div className='product-add-info'>
-                        <span className="font-bold">{`MESA: ${table?.number}`}</span>
-                    </div>
-                    <div>
-                        <span><strong>FECHA:</strong> {moment(payment?.createdAt).format('DD/MM/YYYY HH:mm:ss')}</span>
-                    </div>
-                </div>
 
-                <div className='table-orders-payment' style={{ marginTop: '1.5rem' }}>
-                    <DataTable value={orders && groupOrdersStatus(orders)} >
-                        <Column field="quantity" header="UNIDADES" bodyStyle={{ textAlign: 'center' }}></Column>
-                        <Column field="product.title" header="PRODUCTO" bodyStyle={{ textAlign: 'center' }}></Column>
-                        <Column field="product.price" header="IMPORTE" body={priceBodyTemplate} bodyStyle={{ textAlign: 'center' }}></Column>
-                    </DataTable>
+            <div className='product-add-order'>
+                <div className='product-add-info'>
+                    <span className="font-bold">{`MESA: ${table?.number}`}</span>
                 </div>
+                <div>
+                    <span><strong>FECHA:</strong> {moment(payment?.createdAt).format('DD/MM/YYYY HH:mm:ss')}</span>
+                </div>
+            </div>
 
-                <div style={{ display: 'flex', alignItems: 'center', justifyContent: "space-between", marginTop: "2rem" }}>
-                    <span className="font-bold">MÉTODO DE PAGO:</span>
-                    <i className={"pi pi-credit-card"} style={{fontSize: "1.5rem"}}></i>
-                    <span className="font-bold ml-3">TOTAL: {totalPayment?.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} €</span>
-                </div>
-            </form>
+            <div className='table-orders-payment' style={{ marginTop: '1.5rem' }}>
+                <DataTable value={orders && groupOrdersStatus(orders)} >
+                    <Column field="quantity" header="UNIDADES" bodyStyle={{ textAlign: 'center' }}></Column>
+                    <Column field="product.title" header="PRODUCTO" bodyStyle={{ textAlign: 'center' }}></Column>
+                    <Column field="product.price" header="IMPORTE" body={priceBodyTemplate} bodyStyle={{ textAlign: 'center' }}></Column>
+                </DataTable>
+            </div>
+
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: "space-between", marginTop: "2rem" }}>
+                <span className="font-bold">MÉTODO DE PAGO:</span>
+                <i className={"pi pi-credit-card"} style={{ fontSize: "1.5rem" }}></i>
+                <span className="font-bold ml-3">TOTAL: {totalPayment?.toLocaleString('es-ES', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} €</span>
+            </div>
+
             <div className='mt-4'>
                 <span className="font-bold">Introduce tu tarjeta de crédito:</span>
                 <div className='stripe-payment-container'>
@@ -103,7 +90,7 @@ export function StripePayment(props) {
                 </div>
             </div>
             <div className='btn-stripe-payment'>
-                <Button type="submit" label='Realizar el pago' disabled={!stripe} />
+                <Button label='Realizar el pago' />
             </div>
         </>
     );
