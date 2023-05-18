c86686fe17ab207e0bfd4c6facc7c8e2fcb9e862
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-18 14:38:03 +0200

    Implementación de las peticiónes del pago en el layout

--- src/layouts/ClientLayout/ClientLayout.js
+++ src/layouts/ClientLayout/ClientLayout.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { useAuth, useTable, useOrder } from '../../hooks';
+import { useAuth, useTable, useOrder, usePayment } from '../../hooks';
 import { HomeClient } from '../../pages/Client';
 import { TopMenu, FooterMenu } from '../../components/Client';
+import { size } from 'lodash';
 import './ClientLayout.scss';
 
 export function ClientLayout(props) {
@@ -13,22 +14,17 @@ export function ClientLayout(props) {
     const { authClient } = useAuth();
     const { tables, getTableClient } = useTable();
     const { loading, orders, getOrdersByTableClient } = useOrder();
+    const { getPaymentByIdClient } = usePayment();
 
     const [table, setTable] = useState(null);
     const [ordersTable, setOrdersTable] = useState(null);
+    const [paymentData, setPaymentData] = useState(null);
     const [refreshOrders, setRefreshOrders] = useState(false);
+    const [refreshPayment, setRefreshPayment] = useState(false);
 
     useEffect(() => {
         getTableClient(paramsURL.idTable);
     }, [paramsURL.idTable, getTableClient]);
-
-    useEffect(() => {
-        (async () => {
-            if (table) {
-                getOrdersByTableClient(table.tableBooking?.id);
-            }
-        })();
-    }, [table, getOrdersByTableClient, refreshOrders]);
 
     useEffect(() => {
         if (tables) {
@@ -37,12 +33,32 @@ export function ClientLayout(props) {
     }, [tables]);
 
     useEffect(() => {
+        (async () => {
+            if (tables?.tableBooking) {
+                getOrdersByTableClient(tables.tableBooking.id);
+            }
+        })();
+    }, [tables, getOrdersByTableClient, refreshOrders]);
+
+    useEffect(() => {
         if (orders) {
             setOrdersTable(orders);
         }
     }, [orders]);
 
+    useEffect(() => {
+        if (size(orders) > 0) {
+            if (size(orders[0]?.payment) > 0) {
+                (async () => {
+                    const response = await getPaymentByIdClient(orders[0].payment.id);
+                    setPaymentData(response);
+                })();
+            }
+        }
+    }, [orders, getPaymentByIdClient, refreshPayment]);
+
     const onRefreshOrders = () => setRefreshOrders((prev) => !prev);
+    const onRefreshPayment = () => setRefreshPayment((prev) => !prev);
 
     if (!authClient) {
         return <HomeClient />;
@@ -59,12 +75,13 @@ export function ClientLayout(props) {
                             table: table,
                             orders: ordersTable,
                             onRefreshOrders: onRefreshOrders,
-                            loadingOrders: loading
+                            loadingOrders: loading,
+                            payment: paymentData
                         })}
                 </div>
             </div>
 
-            <FooterMenu idTable={paramsURL.idTable} table={table} orders={ordersTable} />
+            <FooterMenu idTable={paramsURL.idTable} table={table} orders={ordersTable} payment={paymentData} onRefreshPayment={onRefreshPayment} />
         </div>
     )
 }
