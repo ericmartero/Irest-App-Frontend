c1e6df01c63ec335304d013d4c70e9db13d8432c
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-19 17:09:10 +0200

    Creación estilo página principal

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,15 +1,24 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import { Button } from 'primereact/button';
 
 export function MainPage() {
-    
+
+    const history = useHistory();
+
+    const goAdmin = () => {
+        history.push('/admin');
+    };
+
     return (
-        <div className="surface-ground flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden">
+        <div className="flex align-items-center justify-content-center min-h-screen min-w-screen overflow-hidden" style={{backgroundColor: "white"}}>
             <div className="flex flex-column align-items-center justify-content-center">
-                <div style={{ borderRadius: '56px', padding: '0.3rem', background: 'linear-gradient(180deg, rgba(33, 150, 243, 0.4) 10%, rgba(33, 150, 243, 0) 30%)' }}>
-                    <div className="w-full surface-card py-8 px-5 sm:px-8 flex flex-column align-items-center" style={{ borderRadius: '53px' }}>
-                        <span className="text-blue-500 font-bold text-3xl">404</span>
-                        <h1 className="text-900 font-bold text-5xl mb-2">Página no disponible</h1>
-                        <div className="text-600 mb-5">El recurso solicitado no está disponible</div>
+                <div>
+                    <div className="w-full surface-card py-8 px-5 sm:px-8 flex flex-column align-items-center">
+                        <img src="https://res.cloudinary.com/djwjh0wpw/image/upload/v1679673058/icono-irest_gvyksj.png" alt="logo" />
+                        <h1 className="text-900 font-bold text-5xl mb-5" style={{textAlign: "center"}}>Bienvenido a iRest</h1>
+                        <div className="text-600 mb-5" style={{textAlign: "center"}}>Escanea el codigo QR para poder acceder a una mesa</div>
+                        <Button label='Acceder al panel de control' onClick={goAdmin}/>
                     </div>
                 </div>
             </div>
