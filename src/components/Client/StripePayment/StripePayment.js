538ed4aff4e07faa3ed9d8494a6d2023845cba7b
ericmengual <ericmarteroasix@ies-sabadell.cat>
2023-05-21 03:37:45 +0200

    Se implementan mensajes de errores i sistema de carga

--- src/components/Client/StripePayment/StripePayment.js
+++ src/components/Client/StripePayment/StripePayment.js
@@ -1,26 +1,32 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useStripePayment } from '../../../hooks';
 import { CardElement } from '@stripe/react-stripe-js';
 import { Button } from 'primereact/button';
+import { Toast } from 'primereact/toast';
+import { ProgressSpinner } from 'primereact/progressspinner';
 import './StripePayment.scss';
 
 export function StripePayment(props) {
 
     const { totalPayment, createAppPayment } = props;
-    const { checkoutStripe } = useStripePayment();
+
+    const toast = useRef(null);
+    const { loading, checkoutStripe } = useStripePayment();
 
     const makePayment = async () => {
-        try {
-            const response = await checkoutStripe(totalPayment, CardElement);
-            console.log(response);
+        const result = await checkoutStripe(totalPayment, CardElement);
+
+        if (result.error === null) {
             createAppPayment();
-        } catch (error) {
-            console.log(error);
+        } else {
+            toast.current.show({ severity: 'error', summary: 'Operacion Fallida', detail: result.error.message, life: 3000 });
         }
     };
 
     return (
         <>
+            {loading && <ProgressSpinner style={{ position: 'absolute', left: '50%', top: '50%', transform: 'translate(-50%, -50%)', zIndex: 1 }} />}
+            <Toast ref={toast} position="bottom-center" />
             <div>
                 <span className="font-bold">Introduce tu tarjeta de crédito:</span>
                 <div className='stripe-payment-container'>
@@ -38,7 +44,7 @@ export function StripePayment(props) {
                 </div>
             </div>
             <div className='btn-stripe-payment'>
-                <Button label='Pagar' onClick={makePayment}/>
+                <Button label='Pagar' onClick={makePayment} />
             </div>
         </>
     );
